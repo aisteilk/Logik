8fb543d94bb85d50c7985110b61844b1aaf0a91e
aisteilk <aistemariailk@gmail.com>
2020-03-04 11:35:40 +0100

    Beispiel 3 und 4 gerechnet, 4 ausbessern!

--- src/edu/campus02/Main.java
+++ src/edu/campus02/Main.java
@@ -22,8 +22,34 @@ public class Main {
         ichKenneMichAus = true;
 
 
-        ergebnis = dasBeispielVerwirrtmich && ichKenneMichAus;
+        ergebnis = dasBeispielVerwirrtmich || ichKenneMichAus;
 
-        System.out.println("das Beispiel verwirrt mich UND ich kenne mich aus: " + ergebnis);
+        System.out.println("das Beispiel verwirrt mich ODER ich kenne mich aus: " + ergebnis);
+
+        //Bsp 3: (1 > 3 UND (NICHT 3-1 < 5)) ODER 2 == 4*4
+        //       (1 > 3 && (!= 3-1 < 5)) || 2 == 4*4
+
+        boolean teil1;
+        boolean teil2;
+        boolean ergebnis3;
+
+        teil2 = 2==4;
+        teil1 = 1>3 && !(3-1 < 5);
+
+        // Bsp 4
+
+        boolean ergebnis4;
+
+        ergebnis4 = (6 >= 2*3 || 2-3 < 0) && ( 1 * 1 == 1 && 1 != 1+1);
+
+        System.out.println(ergebnis4);
+
+
+
+
+
+
+        ergebnis3 = (1>3 && !(3-1<5))|| 2 == 4;
+        System.out.println(ergebnis3);
     }
 }
